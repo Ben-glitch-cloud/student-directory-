c280bcbde90f12e227d4fe0cb316e6e14ff2200f
Beneidct Lawrence <>
2020-11-17 10:36:44 +0000

    print commit

--- directory.rb
+++ directory.rb
@@ -1,13 +1,19 @@
-puts "The student of Villains Academy"
-puts "------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Varder"
-puts "Nurse Ratched"
-puts "Micheal Corleone"
-puts "Alex DeLarge"
-puts "The Wiched Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+#first we print the list of students
+puts ("The student of Villains Academy")
+puts ("------------")
+puts ("Dr. Hannibal Lecter")
+puts ("Darth Varder")
+puts ("Nurse Ratched")
+puts ("Micheal Corleone")
+puts ("Alex DeLarge")
+puts ("The Wiched Witch of the West")
+puts ("Terminator")
+puts ("Freddy Krueger")
+puts ("The Joker")
+puts ("Joffrey Baratheon")
+puts ("Norman Bates")
+# finally, we print the totoal number of students
+print "Overall, we have "
+#its important that print() doesn't add new line characters 
+print "11"
+print " great students"
